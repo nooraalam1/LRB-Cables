1e029049ec67aa9fcf7a1a316fe9ee968880f6f8
Noor A Alam <noorsomrat2000@gmail.com>
2025-05-18 21:19:05 +0600

    Navbar Updated

--- src/assets/Components/Main.jsx
+++ src/assets/Components/Main.jsx
@@ -2,12 +2,14 @@ import { Outlet } from 'react-router-dom';
 
 import Navbar from './Navbar';
 import Footer from './Footer';
+import ScrollToTop from './ScrollToTop';
 
 const Main = () => {
 
 
     return (
         <div>
+            <ScrollToTop></ScrollToTop>
              <Navbar />
             <Outlet />
             <Footer></Footer>
